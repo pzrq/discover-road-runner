51b757679010fbcdd4698627fca94e8dcf35c833
Peter Schmidt <peter@peterjs.com>
2015-02-18 14:52:40 +1100

    Primitive (no router as no models yet) setup of a second DB so I can investigate issues which arise.

--- discover_road_runner/settings.py
+++ discover_road_runner/settings.py
@@ -67,10 +67,14 @@ WSGI_APPLICATION = 'NOTUSED.wsgi.application'
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
 
 DATABASES = {
+    'acme': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db_acme.sqlite3'),
+    },
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+        'NAME': os.path.join(BASE_DIR, 'db_default.sqlite3'),
+    },
 }
 
 # Internationalization
